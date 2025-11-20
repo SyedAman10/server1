2b75fc9c99f0265aad31994007d0d2742333dfb7
SyedAman10 <amanullahnaqvi@gmail.com>
2025-11-21 00:11:50 +0500

    a

--- routes/aiConfig.routes.js
+++ routes/aiConfig.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const aiConfigController = require('../controllers/aiConfig.controller');
-const { authenticate } = require('../middleware/auth');
+const { authenticate } = require('../middleware/auth.middleware');
 
 /**
  * AI Configuration Routes
